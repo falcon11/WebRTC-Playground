323cb9f09c65e5fd2483dfa4d3cc8a34d0b47dd0
ashoka <savebtc11@gmail.com>
2020-12-07 23:43:45 +0800

    feat: add sepia filter; use requestAnimationFrame as loop engin

--- chat-client/src/pages/cg/index.tsx
+++ chat-client/src/pages/cg/index.tsx
@@ -2,6 +2,19 @@ import { getUserMedia } from '@/services/webrtc-controller';
 import React, { useEffect, useState, useRef } from 'react';
 import styles from './index.less';
 
+const sepiaFilter = function(imgData: ImageData) {
+  let d = imgData.data;
+  for (let i = 0; i < d.length; i += 4) {
+    let r = d[i];
+    let g = d[i + 1];
+    let b = d[i + 2];
+    d[i] = r * 0.393 + g * 0.769 + b * 0.189; // red
+    d[i + 1] = r * 0.349 + g * 0.686 + b * 0.168; // green
+    d[i + 2] = r * 0.272 + g * 0.534 + b * 0.131; // blue
+  }
+  return imgData;
+};
+
 export default () => {
   const [videoStream, setVideoStream] = useState<MediaStream | null>(null);
   const videoRef = useRef<HTMLVideoElement>(null);
@@ -19,23 +32,39 @@ export default () => {
     videoRef.current && (videoRef.current.srcObject = videoStream);
   });
 
+  const renderVideoToCanvas = () => {
+    if (
+      !videoRef.current ||
+      videoRef.current.videoWidth === 0 ||
+      !canvasRef.current
+    ) {
+      return;
+    }
+    const ctx = canvasRef.current.getContext('2d');
+    canvasRef.current.width = videoRef.current.videoWidth;
+    canvasRef.current.height = videoRef.current.videoHeight;
+    ctx?.drawImage(
+      videoRef.current,
+      0,
+      0,
+      videoRef.current.videoWidth,
+      videoRef.current.videoHeight,
+    );
+    const imageData = ctx?.getImageData(
+      0,
+      0,
+      videoRef.current.videoWidth,
+      videoRef.current.videoWidth,
+    );
+    imageData && ctx?.putImageData(sepiaFilter(imageData), 0, 0);
+  };
+
   useEffect(() => {
-    const interval = setInterval(() => {
-      if (!videoRef.current || !canvasRef.current) return;
-      const ctx = canvasRef.current.getContext('2d');
-      canvasRef.current.width = videoRef.current.videoWidth;
-      canvasRef.current.height = videoRef.current.videoHeight;
-      ctx?.drawImage(
-        videoRef.current,
-        0,
-        0,
-        videoRef.current.videoWidth,
-        videoRef.current.videoHeight,
-      );
-    }, 1000 / 30);
-    return () => {
-      clearInterval(interval);
+    const engineLoop = () => {
+      renderVideoToCanvas();
+      requestAnimationFrame(engineLoop);
     };
+    engineLoop();
   }, []);
 
   return (
